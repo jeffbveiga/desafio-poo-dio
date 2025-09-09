3ffea3a87421e26e053919e1b51135d13740e069
Jeff <jeff_contato@hotmail.com>
2025-09-09 18:22:02 -0300

    Adicionado método para verificar os bootcamps concluidos

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,18 +1,20 @@
 package br.com.dio.desafio.dominio;
 
-import java.util.LinkedHashSet;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 public class Dev {
     private String nome;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+    public Map<String, Set> bootcampsInscritos = new LinkedHashMap<>();
+    public List<String> bootcampsConcluidos = new ArrayList<>();;
+
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
         this.conteudosInscritos.addAll(bootcamp.getConteudos());
         bootcamp.getDevsInscritos().add(this);
+
+        this.bootcampsInscritos.put(bootcamp.getNome(), bootcamp.getConteudos());
     }
 
     public void progredir() {
@@ -32,6 +34,26 @@ public class Dev {
                 .sum();
     }
 
+    public void verificarBootcampConcluidos(){
+        boolean concluiuAlgum = false;
+
+        for(Map.Entry<String, Set> entrada : bootcampsInscritos.entrySet()){
+            String chave = entrada.getKey();
+            Set<Conteudo> valores = entrada.getValue();
+
+            if(!conteudosConcluidos.isEmpty() && conteudosConcluidos.containsAll(valores)){
+                System.out.println("Parabéns! Você concluiu o bootcamp: " + chave);
+                concluiuAlgum = true;
+            }
+        }
+
+        if (!concluiuAlgum){
+            System.out.println("Você ainda não concluiu nenhum bootcamp, continue se dedicando!");
+        }
+
+    }
+
+    
     public String getNome() {
         return nome;
     }
